9c1fb3c22db6414f9648b63bb169378d95834a93
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2021-10-22 21:52:50 +0000

    Initial commit

---
+++ notes.md
@@ -0,0 +1,19 @@
+# Notes
+
+- https://atomiks.github.io/tippyjs/
+- https://atomiks.github.io/tippyjs/v5/themes/
+- https://popper.js.org/
+- https://codesandbox.io/s/thirsty-zhukovsky-gpi2w?file=/index.html:32-295
+
+
+```html
+<div class="tippy-popper">
+  <div class="tippy-tooltip" data-placement="top">
+    <div class="tippy-backdrop"></div> <!-- animateFill: true -->
+    <div class="tippy-arrow"></div> <!-- arrow: true -->
+    <div class="tippy-content">
+      My content
+    </div>
+  </div>
+</div>
+```
---
+++ script.js
@@ -0,0 +1,32 @@
+tippy('#left', {
+  placement: 'top',
+  animation: 'fade',
+  theme: 'material',
+  content: '<strong>Details:</strong> <span style="color: aqua;">this is left content</span>',
+  allowHTML: true,
+  maxWidth: 200,
+});
+
+tippy('#right', {
+  placement: 'top',
+  animation: 'fade',
+  theme: 'material',
+  content: '<strong>Details:</strong> <span style="color: lime;">this is right content</span>',
+  allowHTML: true,
+  maxWidth: 200,
+});
+
+// https://atomiks.github.io/tippyjs/v6/html-content/
+
+tippy('.btn', {
+  content(reference) {
+    const id = reference.getAttribute('data-template');
+    const template = document.getElementById(id);
+    return template.innerHTML;
+  },
+  placement: 'top',
+  animation: 'fade',
+  allowHTML: true,
+  theme: 'material',
+  maxWidth: 250,
+});
